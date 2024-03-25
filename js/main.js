6680afe98efc7e14613797de3ed5af354181b3f5
Saeed Abdollahi <saeed.abdollahi7878@gmail.com>
2024-03-25 13:13:10 +0330

    add all the assets

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const collpasibles = document.querySelectorAll(".collapsible");
+collpasibles.forEach((item) =>
+    item.addEventListener("click", function(){
+        this.classList.toggle("collapsible--expanded");
+    })
+);    
